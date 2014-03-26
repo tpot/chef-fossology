328d40b68c559c9f6df1209bf38730b0fcc50ae3
Tim Potter <tpot@hp.com>
2014-03-26 16:52:00 +1100

    Install and configure apache2

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+# Install fossology
+
 apt_repository "fossology" do
   uri "http://www.fossology.org/releases/2.4.0/Ubuntu/"
   distribution "12.04"
@@ -15,3 +17,22 @@ apt_repository "fossology" do
 end
 
 package "fossology"
+
+# Remove apache2 site stuff installed by package
+
+["/etc/apache2/sites-enabled/fossology.conf",
+ "/etc/apache2/sites-available/fossology.conf"].each do |f|
+  file f do
+    action :delete
+  end
+end
+
+# Install and configure apache
+
+include_recipe "apache2"
+
+web_app "chef-fossology" do
+  server_name "fossology.mon.hpcloud.net"
+  server_aliases ["fossology.mon.hpcloud.net"]
+  docroot "/usr/share/fossology/www/ui"
+end
